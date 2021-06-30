8591d8a5526526a920245546fda917cf6de2ee39
이수진 <lsj4329@i-commtech.com>
2021-06-30 13:54:20 +0900

    loginCheck 메소드 추가

--- src/main/java/com/example/SpringLogin/mapper/MemberMapper.java
+++ src/main/java/com/example/SpringLogin/mapper/MemberMapper.java
@@ -1,5 +1,8 @@
 package com.example.springLogin.mapper;
 
+import com.example.springLogin.vo.MemberVO;
+
 public interface MemberMapper {
     int dbCheck();
+    MemberVO loginCheck(MemberVO vo);
 }
